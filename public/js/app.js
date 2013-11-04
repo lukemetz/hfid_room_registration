4e760391ad62a3d471f4a11d7409a58efe92dae8
= <luke.metz@students.olin.edu>
2013-11-04 21:17:41 +0000

    Updated Angular to 1.2

--- public/js/app.js
+++ public/js/app.js
@@ -2,12 +2,16 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives']).
-  config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
-    $routeProvider.when('/home', {templateUrl: 'partial/home', controller: HomeController});
-    $routeProvider.when('/calendar', {templateUrl: 'partial/calendar', controller: CalController});
-    $routeProvider.when('/confirm', {templateUrl: 'partial/confirm', controller: ConfirmController});
-    $routeProvider.when('/roomSelect', {templateUrl: 'partial/roomselect', controller:RoomSelectController});
+angular.module('myApp', [
+    'ngRoute',
+    'myApp.filters',
+    'myApp.services',
+    'myApp.directives',
+    'myApp.controllers']).
+  config(['$routeProvider', function($routeProvider, $locationProvider) {
+    $routeProvider.when('/home', {templateUrl: 'partial/home', controller: "HomeController"});
+    $routeProvider.when('/calendar', {templateUrl: 'partial/calendar', controller: "CalController"});
+    $routeProvider.when('/confirm', {templateUrl: 'partial/confirm', controller: "ConfirmController"});
+    $routeProvider.when('/roomSelect', {templateUrl: 'partial/roomselect', controller:"RoomSelectController"});
     $routeProvider.otherwise({redirectTo: '/home'});
-    $locationProvider.html5Mode(true);
-  }]);
+  }])
--- public/js/controllers.js
+++ public/js/controllers.js
@@ -2,7 +2,8 @@
 
 /* Controllers */
 
-function AppCtrl($scope, $http) {
+angular.module("myApp.controllers", []).
+  controller("AppCtrl", ["$scope", "$http", function($scope, $http) {
   $http({method: 'GET', url: '/api/name'}).
   success(function(data, status, headers, config) {
     $scope.name = data.name;
@@ -10,39 +11,19 @@ function AppCtrl($scope, $http) {
   error(function(data, status, headers, config) {
     $scope.name = 'Error!'
   });
-}
 
-function MyCtrl1() {}
-MyCtrl1.$inject = [];
-
-var HomeController = function($scope) {
-  $scope.room = "any";
-  $scope.room = "an33y";
-  console.log("in home")
-
-}
-
-var CalController = function($scope) {
-  $scope.room = "";
-  $scope.weekly = false;
-  console.log("in calendar")
-
-}
-
-
-function MyCtrl2() {
-}
-MyCtrl2.$inject = [];
-
-var ConfirmController = function($scope) {
-}
-var RoomSelectController = function($scope, RoomsFactory, defaultFilter) {
-  $scope.rooms = RoomsFactory.getRooms();
-  $scope.filter = defaultFilter;
-  $scope.validRooms = function(room) {
-    console.log(room);
-    return true;
-  };
-  console.log($scope.filter);
-
-}
+  }])
+  .controller("HomeController", ["$scope", function($scope) {
+    $scope.room = "any";
+    $scope.room = "an33y";
+  }])
+  .controller("CalController", ["$scope", function($scope) {
+    $scope.room = "";
+    $scope.weekly = false;
+  }])
+  .controller("ConfirmController", ["$scope", function($scope) {
+  }])
+  .controller("RoomSelectController", ["$scope", "RoomsFactory", "defaultFilter",  function($scope, RoomsFactory, defaultFilter) {
+    $scope.rooms = RoomsFactory.getRooms();
+    $scope.filter = defaultFilter;
+  }]);
