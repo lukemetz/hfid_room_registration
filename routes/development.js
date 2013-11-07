b17a23879237971b0eace13be1d61ee9a41079f7
= <luke.metz@students.olin.edu>
2013-11-07 15:03:00 +0000

    api for rooms done

--- routes/development.js
+++ routes/development.js
@@ -1,26 +1,28 @@
 var models = require("../models.js");
 
 exports.populate = function(req, res){
-  var AC109 = new models.Room({name: "AC 109", 
-                            BlackoutCapability: false,
-                            HandicapAccessability: false,
-                            Softseating: false,
-                            TablesChairs: true,
-                            Whiteboards: false,
-                            Blackboards: true,
-                            ProjectionScreen: true,
-                            LCDProjector: true,
-                            CarpetFlooring: false,
-                            TileFlooring: true,
-                            Multimedia: false,
-                            Benches: false,
-                            Podium: false,
-                            TieredSeating: false,
-                            DoNotRearrange: true});
-    AC109.save(function(err){
-        if (err) return ("error saving AC109", err);
-        console.log('AC109 saved');
-    });
+  for (var i =0; i < 10; i++) {
+    var AC109 = new models.Room({name: "AC 10" + i.toString(),
+                              BlackoutCapability: false,
+                              HandicapAccessability: false,
+                              Softseating: false,
+                              TablesChairs: true,
+                              Whiteboards: false,
+                              Blackboards: true,
+                              ProjectionScreen: true,
+                              LCDProjector: true,
+                              CarpetFlooring: false,
+                              TileFlooring: true,
+                              Multimedia: false,
+                              Benches: i%4,
+                              Podium: false,
+                              TieredSeating: i%2,
+                              DoNotRearrange: true});
+      AC109.save(function(err){
+          if (err) return ("error saving AC109", err);
+          console.log(i.toString() + ' saved');
+      });
+  }
 
-  res.send("Database Populated");  
+  res.send("Database Populated");
 };
